0e320f40ed0964e71737eada1308c9f7e28a68be
code <codecode60@protonmail.com>
2022-10-31 12:41:48 +0930

    Working on navbar

--- src/navbar.rs
+++ src/navbar.rs
@@ -1,5 +1,5 @@
-use yew::{function_component, Html, html, Callback};
-use web_sys::{Window, console, window};
+use yew::{function_component, Html, html};
+use web_sys::{ console, window};
 
 #[function_component(Navbar)]
 pub fn return_navbar() -> Html {
@@ -8,21 +8,32 @@ pub fn return_navbar() -> Html {
 
     html! { 
         <div class="navbar">
-            <div class="navbar-home">
+            <a class="navbar-home">
             <div class="navbar-home-name"> 
                 {"CODY ROSS"}
             </div>
             <div class="navbar-home-title">
                 {"SOFTWARE DEVELOPER"}
             </div>
-            </div>
+            </a>
             <div class="navbar-tail">
-                <div class="navbar-resume">{"Resume"}</div>
-                <div class="navbar-github">{"Github"}</div>
-                <div 
-                    onclick={move |_| {console::log_1(&format!("{:?}", wind.inner_width()).into())}} 
-                    class="navbar-contact">{"Contact"}
-                </div>
+                <a class="navbar-resume word" href="/assets/Cody Ross - Resume - 031022.pdf" target="_blank">{"RESUME"}</a>
+                <a class="navbar-github word" href="https://github.com/co-decode" target="_blank">
+                {"GITHUB"}</a>
+                <a onclick={move |_| {console::log_1(&format!("{:?}", wind.inner_width()).into())}} 
+                   class="navbar-contact word" href="#contact">
+                {"CONTACT"}</a>
+                /* SVG for < 420px */
+                <a class="navbar-resume img">
+					<img src="/assets/images/resume.svg" alt="" />
+				</a>
+
+                <a class="navbar-github img">
+					<img src="/assets/images/github.svg" alt="" />
+				</a>
+                <a class="navbar-contact img">
+					<img src="/assets/images/email1.svg" alt="" />
+				</a>
             </div>
         </div>
     }
